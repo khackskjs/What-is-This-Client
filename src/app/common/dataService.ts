4b054d488aa7aad56decb5cfde6cd212c496f15c
khackskjs <khackskjs@gmail.com>
2018-03-06 22:32:35 +0300

    DataService getCards() 추가

--- src/app/common/dataService.ts
+++ src/app/common/dataService.ts
@@ -2,6 +2,8 @@ import { Http } from "@angular/http";
 import UserInput from "../data-model/UserInput";
 import { Injectable } from "@angular/core";
 
+const SERVER_URL = 'http://localhost:8000';
+
 @Injectable()
 export default class DataService {
   constructor(private http: Http) {
@@ -9,7 +11,13 @@ export default class DataService {
 
   public addNewCard(userInput: UserInput) {
     console.log('addNewCard ', userInput);
-    this.http.post('http://localhost:8000/card', userInput)
-    .subscribe(results => console.log(results));
+    this.http.post(`${SERVER_URL}/card`, userInput)
+      .subscribe(results => console.log(results));
+  }
+
+  public getCards(params) {
+    console.log('getCards')
+    this.http.get(`${SERVER_URL}/card`, { params: params })
+      .subscribe(results => {console.log('getCards', results);});
   }
 }
